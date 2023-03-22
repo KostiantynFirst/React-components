76a5433bf0037d05983ce9c930ef4e7cdd385ada
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-03-22 22:37:05 +0200

    Sum

--- my-app/src/components/Event/Event.jsx
+++ my-app/src/components/Event/Event.jsx
@@ -1,28 +1,29 @@
 import {FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock} from 'react-icons/fa'
 import PropTypes from 'prop-types';
 import css from './Event.module.css'
-import { formatEventStart, formatEventDuration } from "utils/index";
+import { iconSize } from "constants";
+import { formatEventStart, formatEventDuration } from "utils";
 
 export const Event = ({name, location, speaker, type, start, end}) => {
     return <div className={css.event}>
     <h2 className={css.title}>{name}</h2>
     <p className={css.info}>
-      <FaMapMarkerAlt className={css.icon}/>
+      <FaMapMarkerAlt className={css.icon} size={iconSize.sm}/>
       {location}
     </p>
     <p className={css.info}>
-      <FaUserAlt className={css.icon} />
+      <FaUserAlt className={css.icon} size={iconSize.sm}/>
       {speaker}
     </p>
     <p className={css.info}>
-      <FaCalendarAlt className={css.icon} />
+      <FaCalendarAlt className={css.icon} size={iconSize.sm}/>
       {formatEventStart(start)}
     </p>
     <p className={css.info}>
       <FaClock className={css.icon}/>
       {formatEventDuration(start, end)}
     </p>
-    {/* <span className="chip free|paid|vip">Event type</span> */}
+    <span className={`${css.chip} ${css[type]}`}>{type}</span>
   </div>
 }
 
---
+++ my-app/src/constants/iconsSize.js
@@ -0,0 +1,5 @@
+export const iconSize = Object.freeze({
+    sm: 24,
+    md: 24,
+    lg: 32
+})
