2b5fa16bb194b46be792dc4691e9c338e2c4534b
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-04-19 13:03:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import { ThemeProvider } from "@emotion/react";
 import './index.css';
 import {App} from 'components/App/App';
-import { theme } from "constants";
+// import { theme } from "constants";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
